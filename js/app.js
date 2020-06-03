9e2a212bddc06ced31529766923547bf1f333761
vinay8487 <49742703+vinay8487@users.noreply.github.com>
2020-06-03 13:54:45 +0530

    Files added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var i = 0;
+var txt = 'Wishing U a very Happy Birthday dear Shalini!'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+var newline='<br/>';
+var emoji='&#128522;';
+var count=0;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }else{
+      if(count==0){
+        document.getElementById("demo").innerHTML +=newline;
+        document.getElementById("demo").innerHTML += emoji;
+        count++;
+      }
+  }
+}
