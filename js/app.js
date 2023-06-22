e8a2c83756650639c5073215326a29ede5099b30
vinay8487 <49742703+vinay8487@users.noreply.github.com>
2023-06-22 20:40:14 +0530

    Varenya 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Wishing a very Happy Birthday to Varenya ki mamma'; /* The text dear XXX! */
+var txt = 'Wishing a very Happy Birthday to Varenya ki Mumma'; /* The text dear XXX! */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 var newline='<br/>';
 var emoji='&#128522;'; //128591
